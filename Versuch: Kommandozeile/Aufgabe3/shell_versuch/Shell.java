550f0407a2e475b16d99bb49dde114a8f9a0318e
Henning-Klatt <henning.klatt@web.de>
2022-11-09 16:57:18 +0100

    Aufgabe 3 Shell >

--- Versuch: Kommandozeile/Aufgabe3/shell_versuch/Shell.java
+++ Versuch: Kommandozeile/Aufgabe3/shell_versuch/Shell.java
@@ -5,36 +5,41 @@ import java.io.File;
 
 
 class Shell {
+
+    // Execute single command without && or > < -
+    public static int execute(String command){
+        String[] argumente = command.split(" ");
+
+        File f = new File(argumente[0]);
+        if (!f.exists() || f.isDirectory()) {
+            System.out.println("Datei (" + argumente[0] + ") nicht vorhanden");
+            exit(0);
+        }
+
+        if (!f.canExecute()) {
+            System.out.println("Datei nicht ausführbar");
+            exit(0);
+        }
+
+        int result = execv(argumente[0], argumente);
+        return result;
+    }
+
     public static void main(String[] args) {
 
         while (true) {
             System.out.print("> ");
             String input = System.console().readLine();
-
             String[] commands = input.split(" && ");
 
             for (int i = 0; i < commands.length; i++) {
 
+                String command = commands[i];
 
-                String[] command = commands[i].split(" ");
-                System.out.println(Arrays.toString(command));
-
-                if (command[0].equals("exit")) {
+                if (command.equals("exit")) {
                     System.out.println("Bye! :)");
                     exit(0);
                 }
-
-                File f = new File(command[0]);
-                if (!f.exists() || f.isDirectory()) {
-                    System.out.println("Datei nicht vorhanden");
-                    break;
-                }
-
-                if (!f.canExecute()) {
-                    System.out.println("Datei nicht ausführbar");
-                    break;
-                }
-
 
                 int pids = fork();
 
@@ -46,7 +51,32 @@ class Shell {
                 if (pids == 0) {
 
                     System.out.println("Child: " + pids);
-                    int result = execv(command[0], command);
+
+                    // cat abc > xyz    # wirkt wie "cp abc xyz"
+                    if(command.contains(">")){
+                        String[] splitArray = command.split(" > ");
+                        // int open(String path, int flags)
+                        int fd = open(splitArray[splitArray.length-1], O_WRONLY | O_CREAT);
+                        System.out.println("Öffne Datei: " + splitArray[splitArray.length-1]);
+                        int result = execute(splitArray[0]);
+                        // Ergebnis von execute in buf schreiben
+                        byte[] buf;
+                        int read(1, byte[] buf, int count)
+                        // int write(int fd, byte[] buf, int count)
+                        int writeresult = write(fd, )
+
+                    }
+                    if(command.contains("<")){
+                        String[] splitArray = command.split(" < ");
+                        int fd = open(splitArray[0], O_RDONLY);
+
+                    }
+                    if(command.contains("-")){
+                        String[] splitArray = command.split(" - ");
+
+                    }
+
+                    int result = execute(command);
                     System.out.println("Result: " + result);
                     exit(0);
                 }
